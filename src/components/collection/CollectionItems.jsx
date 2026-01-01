b69a717308b980ef16eccce26000ec9457504a54
Tevin Crite <crite2004@gmail.com>
2026-01-01 00:00:07 -0500

    Made Collection page dynamic and implented skeleton loading state

--- src/components/collection/CollectionItems.jsx
+++ src/components/collection/CollectionItems.jsx
@@ -1,10 +1,38 @@
 import { faEye } from "@fortawesome/free-regular-svg-icons";
 import { faShoppingBag } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import Skeleton from "../ui/Skeleton";
 
-export default function CollectionItems() {
+export default function CollectionItems({ collection, loading }) {
+  const [listLength, setListLength] = useState(12);
+  const [sort, setSort] = useState("");
+  const [sortedItemList, setSortedItemList] = useState([collection?.items]);
+
+  function sortItemList() {
+    if (sort === "HIGH_TO_LOW") {
+      setSortedItemList(
+        collection?.items.slice().sort((a, b) => b.price - a.price)
+      );
+    } else if (sort === "LOW_TO_HIGH") {
+      setSortedItemList(
+        collection?.items.slice().sort((a, b) => a.price - b.price)
+      );
+    } else {
+      setSortedItemList(collection?.items);
+    }
+    console.log(collection?.items);
+  }
+
+  // useEffect(() => {
+  //   sortItemList()
+  // }, [])
+
+  useEffect(() => {
+    sortItemList();
+  }, [sort]);
+
   return (
     <section id="collection-items">
       <div className="row collection-items__row">
@@ -15,47 +43,83 @@ export default function CollectionItems() {
               Live
             </span>
             <span className="collection-items__header__results">
-              10 results
+              {collection?.items.length} results
             </span>
           </div>
-          <select className="collection-items__header__sort">
-            <option value="" default>
+          <select
+            value={sort}
+            className="collection-items__header__sort"
+            onChange={(event) => setSort(event.target.value)}
+          >
+            <option value={""} disabled>
               Default
             </option>
-            <option value="">Price high to low</option>
-            <option value="">Price low to high</option>
+            <option value="HIGH_TO_LOW">Price high to low</option>
+            <option value="LOW_TO_HIGH">Price low to high</option>
           </select>
         </div>
         <div className="collection-items__body">
-          {new Array(8).fill(0).map((_, index) => (
-            <div className="item-column">
-              <Link to={"/item"} key={index} className="item">
-                <figure className="item__img__wrapper">
-                  <img
-                    src="https://i.seadn.io/gcs/files/0a085499e0f3800321618af356c5d36b.png?auto=format&dpr=1&w=384"
-                    alt=""
-                    className="item__img"
-                  />
-                </figure>
-                <div className="item__details">
-                  <span className="item__details__name">Meebit #0001</span>
-                  <span className="item__details__price">0.98 ETH</span>
-                  <span className="item__details__last-sale">
-                    Last sale: 7.45 ETH
-                  </span>
-                </div>
-                <div className="item__see-more">
-                  <button className="item__see-more__button">See More</button>
-                  <div className="item__see-more__icon">
-                    <FontAwesomeIcon icon={faShoppingBag} />
+          {loading
+            ? new Array(12).fill(0).map((_, index) => (
+                <div key={index} className="item-column">
+                  <div key={index} className="item">
+                    <figure className="item__img__wrapper">
+                      <Skeleton width="100%" height="100%" borderRadius="4px" />
+                    </figure>
+                    <div className="item__details">
+                      <span className="item__details__name">
+                        <Skeleton
+                          width="150px"
+                          height="16px"
+                          borderRadius="4px"
+                        />
+                      </span>
+                      <span className="item__details__price">
+                        <Skeleton width="50px" height="16px" borderRadius="4px" />
+                      </span>
+                      <span className="item__details__last-sale">
+                        <Skeleton width="130px" height="16px" borderRadius="4px" />
+                      </span>
+                    </div>
                   </div>
                 </div>
-              </Link>
-            </div>
-          ))}
+              ))
+            : sortedItemList?.slice(0, listLength).map((e, index) => (
+                <div key={index} className="item-column">
+                  <Link to={"/item"} key={index} className="item">
+                    <figure className="item__img__wrapper">
+                      <img src={e?.imageLink} alt="" className="item__img" />
+                    </figure>
+                    <div className="item__details">
+                      <span className="item__details__name">{e?.title}</span>
+                      <span className="item__details__price">
+                        {e?.price} ETH
+                      </span>
+                      <span className="item__details__last-sale">
+                        Last sale: {e?.lastSale} ETH
+                      </span>
+                    </div>
+                    <div className="item__see-more">
+                      <button className="item__see-more__button">
+                        See More
+                      </button>
+                      <div className="item__see-more__icon">
+                        <FontAwesomeIcon icon={faShoppingBag} />
+                      </div>
+                    </div>
+                  </Link>
+                </div>
+              ))}
         </div>
       </div>
-      <button className="collection-page__button">Load more</button>
+      {listLength < collection?.items.length && (
+        <button
+          className="collections-page__button"
+          onClick={() => setListLength(listLength + 6)}
+        >
+          Load more
+        </button>
+      )}
     </section>
   );
 }
--- src/pages/CollectionPage.jsx
+++ src/pages/CollectionPage.jsx
@@ -1,18 +1,42 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import CollectionHeader from "../components/collection/CollectionHeader";
 import CollectionInfo from "../components/collection/CollectionInfo";
 import CollectionItems from "../components/collection/CollectionItems";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 export default function CollectionPage() {
+  const { id } = useParams();
+  const [collection, setCollection] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  async function fetchCollection() {
+    try {
+      const { data } = await axios.get(
+        `https://remote-internship-api-production.up.railway.app/collection/${id}`
+      );
+
+      const collectionData = data.data;
+
+      setCollection(collectionData);
+
+      setLoading(false);
+    } catch (error) {
+      alert(error);
+    }
+  }
+
   useEffect(() => {
+    fetchCollection();
+    setLoading(true);
     window.scrollTo(0, 0);
   }, []);
 
   return (
     <>
-      <CollectionHeader />
-      <CollectionInfo />
-      <CollectionItems />
+      <CollectionHeader collection={collection} loading={loading} />
+      <CollectionInfo collection={collection} loading={loading} />
+      {collection && <CollectionItems collection={collection} loading={loading} />}
     </>
   );
 }
