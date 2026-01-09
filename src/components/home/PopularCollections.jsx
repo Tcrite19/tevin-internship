55111f50e92c5d4c3487080803c0eee8ffdf3b3f
Tevin Crite <crite2004@gmail.com>
2026-01-08 20:03:05 -0500

    Fix: Extra space removed between title and carousel for popular and new collection components

--- src/components/home/PopularCollections.jsx
+++ src/components/home/PopularCollections.jsx
@@ -6,7 +6,7 @@ import "swiper/css";
 import CollectionCard from "../ui/CollectionCard";
 import CollectionCardSkeleton from "../ui/CollectionCardSkeleton";
 import AOS from "aos";
-import 'aos/dist/aos.css';
+import "aos/dist/aos.css";
 
 export default function PopularCollections() {
   const [popularCollection, setPopularCollection] = useState([]);
@@ -43,57 +43,53 @@ export default function PopularCollections() {
           <h2 className="popular-collections__title" data-aos="fade-up">
             Popular Collections
           </h2>
-          <div
-            className="popular-collections__body"
+          <Swiper
             data-aos="fade-up"
             data-aos-delay="100"
+            navigation={true}
+            modules={[Navigation]}
+            slidesPerView={6}
+            spaceBetween={16}
+            loop={true}
+            breakpoints={{
+              0: {
+                width: 0,
+                slidesPerView: 1,
+              },
+              368: {
+                width: 368,
+                slidesPerView: 1,
+              },
+              768: {
+                width: 768,
+                slidesPerView: 2,
+              },
+              1024: {
+                width: 992,
+                slidesPerView: 3,
+              },
+              1220: {
+                width: 1220,
+                slidesPerView: 4,
+              },
+              1440: {
+                width: 1440,
+                slidesPerView: 5,
+              },
+            }}
           >
-            <Swiper
-              navigation={true}
-              modules={[Navigation]}
-              slidesPerView={6}
-              spaceBetween={16}
-              loop={true}
-              breakpoints={{
-                0: {
-                  width: 0,
-                  slidesPerView: 1,
-                },
-                368: {
-                  width: 368,
-                  slidesPerView: 1,
-                },
-                768: {
-                  width: 768,
-                  slidesPerView: 2,
-                },
-                1024: {
-                  width: 992,
-                  slidesPerView: 3,
-                },
-                1220: {
-                  width: 1220,
-                  slidesPerView: 4,
-                },
-                1440: {
-                  width: 1440,
-                  slidesPerView: 5,
-                },
-              }}
-            >
-              {loading
-                ? new Array(9).fill(0).map((_, index) => (
-                    <SwiperSlide key={index}>
-                      <CollectionCardSkeleton />
-                    </SwiperSlide>
-                  ))
-                : popularCollection.map((e, index) => (
-                    <SwiperSlide key={index}>
-                      <CollectionCard collection={e} id={"collectionId"} />
-                    </SwiperSlide>
-                  ))}
-            </Swiper>
-          </div>
+            {loading
+              ? new Array(9).fill(0).map((_, index) => (
+                  <SwiperSlide key={index}>
+                    <CollectionCardSkeleton />
+                  </SwiperSlide>
+                ))
+              : popularCollection.map((e, index) => (
+                  <SwiperSlide key={index}>
+                    <CollectionCard collection={e} id={"collectionId"} />
+                  </SwiperSlide>
+                ))}
+          </Swiper>
         </div>
       </div>
     </section>
